49dac3cf1f66efcdb36c8aaab66e0262a263b493
Ismail <nitec_427@hotmail.com>
2023-02-23 15:40:31 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  // To-do
+  // Center your time component
+  //
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen bg-purple-400 flex justify-center">
+      {/* Card Wrapper */}
+      <div className="h-screen w-1/3 bg-red-600">Dear Maria</div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#c24638",
+        "primary-light": "#c76156",
+      },
+    },
+  },
+  plugins: [],
+};
